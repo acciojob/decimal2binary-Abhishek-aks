df7d74931a7fef6014f6d87a47a39e4145705f24
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 05:20:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function decimalToBinary(num) {
   //Write you code here for binary conversion
 	let b ="";                // Empty element
-	for (let i =0; i<= num.length, i++){
-		let d1 = Math.Floor(num/2);
+	while (num > 0){
+		
+		let d1 = num%2;
+		num =  Math.Floor(num/2);
 		b += d1;
 	}
 	return b;
