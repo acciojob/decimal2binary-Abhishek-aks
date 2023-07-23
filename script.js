10e0e3251a01fc14c1645235c972f1e96763b303
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 05:26:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function decimalToBinary(num) {
 	let b ="";                // Empty element
 	while (num > 0){
 		let d1 = num%2;            // remainder
+		// console.log(d1);
 		num =  Math.Floor(num/2);  // quotient
+		// console.log(num);
 		b = d1 +b;
+		// console.log(b);
 	}
 	return b;
   
