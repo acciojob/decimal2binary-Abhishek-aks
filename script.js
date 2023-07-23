71fb8aab2a51f90f16704d0cf7aac07a55fa52ac
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 05:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
-  //Write you code here
-	return binary(num)
+  //Write you code here for binary conversion
+	let b ="";                // Empty element
+	for (let i =0; i<= num.length, i++){
+		let d1 = Math.Floor(num/2);
+		b += d1;
+	}
+	return b;
   
 }
 
