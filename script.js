6bca6e9458c7595b91e93daf05dfba2e62734c5c
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 08:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function decimalToBinary(num) {
   //Write you code here for binary conversion
 	let binary ="";                // Empty element
 	while (num > 0){
-		binary = (num%2)+ binary;      // adding remainder
+		binary = (num%2)+ String(binary);      // adding remainder
 		num =  Math.floor(num/2);      // quotient
 	}
 	return binary;
